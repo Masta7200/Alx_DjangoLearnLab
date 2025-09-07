7635fe68293b2de533e843e94d01a61bf7b5d0c8
moustapha7200@gmail.com <moustapha7200@gmail.com>
2025-09-07 22:20:04 +0200

    new files added

---
+++ django-models/LibraryProject/relationship_app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.views import View
+from .models import Book, Library
+
+# Function-based view to list all books
+def list_books(request):
+    books = Book.objects.all()  # Retrieve all books from the database
+    return render(request, 'list_books.html', {'books': books})
+
+# Class-based view to display details for a specific library
+class LibraryDetailView(View):
+    def get(self, request, pk):
+        library = Library.objects.get(pk=pk)  # Retrieve the library by primary key
+        return render(request, 'library_detail.html', {'library': library})
