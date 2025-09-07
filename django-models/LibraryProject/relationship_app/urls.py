dadd4c74b3b53d5319c9d692b56e30652dce6b7d
moustapha7200@gmail.com <moustapha7200@gmail.com>
2025-09-07 22:57:30 +0200

    commit added

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,11 @@
-from django.contrib import admin
-from django.urls import path, include
-from .views import list_books, LibraryDetailView
+# relationship_app/urls.py
+from django.urls import path
+from .views import list_books, LibraryDetailView, RegisterView, LoginView, LogoutView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('relationship/', include('relationship_app.urls')),  # Include the relationship_app URLs
+    path('books/', list_books, name='list_books'),  # Example book listing URL
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # Detail view for library
+    path('register/', RegisterView.as_view(), name='register'),  # Registration URL
+    path('login/', LoginView.as_view(), name='login'),  # Login URL
+    path('logout/', LogoutView.as_view(), name='logout'),  # Logout URL
 ]
