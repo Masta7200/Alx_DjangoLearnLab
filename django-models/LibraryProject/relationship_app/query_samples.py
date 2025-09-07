bc9af79228e02d55bb3afe089942d53aa5705800
moustapha7200@gmail.com <moustapha7200@gmail.com>
2025-09-07 22:12:29 +0200

    commit added

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -10,16 +10,28 @@ django.setup()
 from relationship_app.models import Author, Book, Library, Librarian
 
 def query_books_by_author(author_name):
-    books = Book.objects.filter(author__name=author_name)
-    return books
+    try:
+        author = Author.objects.get(name=author_name)  # Retrieve the Author object
+        books = Book.objects.filter(author=author)  # Filter books by the retrieved Author
+        return books
+    except Author.DoesNotExist:
+        return f"No author found with the name '{author_name}'."
 
 def list_books_in_library(library_name):
-    library = Library.objects.get(name=library_name)
-    return library.books.all()
+    try:
+        library = Library.objects.get(name=library_name)  # Retrieve the Library object
+        return library.books.all()  # Return all books in the library
+    except Library.DoesNotExist:
+        return f"No library found with the name '{library_name}'."
 
 def retrieve_librarian_for_library(library_name):
-    library = Library.objects.get(name=library_name)
-    return library.librarian
+    try:
+        library = Library.objects.get(name=library_name)  # Retrieve the Library object
+        return library.librarian  # Return the associated Librarian
+    except Library.DoesNotExist:
+        return f"No library found with the name '{library_name}'."
+    except Librarian.DoesNotExist:
+        return f"No librarian found for the library '{library_name}'."
 
 if __name__ == "__main__":
     # Example usage
