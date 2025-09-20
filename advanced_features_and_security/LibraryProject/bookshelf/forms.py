30480f8a77ec09ff3d2963114ae89e079e4a0ab5
Masta7200 <131053603+Masta7200@users.noreply.github.com>
2025-09-20 14:46:06 +0200

    Update forms.py

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,1 +1,6 @@
-
+<!-- Example form in form_example.html -->
+<form method="post" action="{% url 'your_view_name' %}">
+    {% csrf_token %}
+    <!-- Your form fields here -->
+    <button type="submit">Submit</button>
+</form>
