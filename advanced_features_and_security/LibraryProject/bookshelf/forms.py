fc8b64522c3d00226806c6b3ef9399f78ac48af3
Masta7200 <131053603+Masta7200@users.noreply.github.com>
2025-09-20 14:49:55 +0200

    Update forms.py

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,14 +1,12 @@
-from django import forms
-from .models import Book
+from django.shortcuts import render, redirect
+from .forms import ExampleForm
 
-class BookForm(forms.ModelForm):
-    class Meta:
-        model = Book
-        fields = ['title', 'author', 'publication_year']  # Specify the fields to include in the form
-
-    def clean_publication_year(self):
-        # Example of custom validation for the publication year
-        year = self.cleaned_data.get('publication_year')
-        if year < 0:
-            raise forms.ValidationError("Publication year cannot be negative.")
-        return year
+def example_view(request):
+    if request.method == 'POST':
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            # Process the data in form.cleaned_data
+            return redirect('success_url')  # Redirect after successful submission
+    else:
+        form = ExampleForm()
+    return render(request, 'bookshelf/example_form.html', {'form': form})
