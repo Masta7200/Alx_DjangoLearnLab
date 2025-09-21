01be24bb5f63e64846ad7a8f6169dbb9efadcb05
moustapha7200@gmail.com <moustapha7200@gmail.com>
2025-09-21 22:17:06 +0200

    added

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from django.urls import path, include  # Import include here
+from django.urls import path, include
+
+from api.views import BookList  # Import include here
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),  # Include the api.urls here
+    path('books/', BookList.as_view(), name='book-list'),
 ]
