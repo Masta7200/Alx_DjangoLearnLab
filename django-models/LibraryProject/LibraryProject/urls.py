09a5840d93ca74a7be5ca3e71db1c52ce052a7f5
moustapha7200@gmail.com <moustapha7200@gmail.com>
2025-09-21 21:46:10 +0200

    new commit added

--- django-models/LibraryProject/LibraryProject/urls.py
+++
@@ -1,8 +0,0 @@
-# LibraryProject/urls.py
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('relationship/', include('relationship_app.urls')),  # Include the relationship_app URLs
-]
