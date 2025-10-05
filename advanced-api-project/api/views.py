bf4967f74d3cd05b8f217f8ec3e226113ea128e5
moustapha7200@gmail.com <moustapha7200@gmail.com>
2025-10-05 23:56:18 +0200

    new commit

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -7,7 +7,7 @@ class BookListView(generics.ListCreateAPIView):
     """View to list all books and create a new book."""
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [AllowAny]  # Allow any user to view the list
+    permission_classes = [AllowAny]  # type: ignore # Allow any user to view the list
 
 class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
     """View to retrieve, update, or delete a book."""
