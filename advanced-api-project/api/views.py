40bad70ecb78983c841ee317626b846e767f8151
moustapha7200@gmail.com <moustapha7200@gmail.com>
2025-10-05 23:57:32 +0200

    new commit

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
 
@@ -7,7 +7,7 @@ class BookListView(generics.ListCreateAPIView):
     """View to list all books and create a new book."""
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [AllowAny]  # type: ignore # Allow any user to view the list
+    permission_classes = [AllowAny]  # Allow any user to view the list
 
 class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
     """View to retrieve, update, or delete a book."""
