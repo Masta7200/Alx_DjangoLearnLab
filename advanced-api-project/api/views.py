3e656e82221cc2605e5979233608754fb40b92ab
moustapha7200@gmail.com <moustapha7200@gmail.com>
2025-10-05 23:54:25 +0200

    new commit

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+class BookListView(generics.ListCreateAPIView):
+    """View to list all books and create a new book."""
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [AllowAny]  # Allow any user to view the list
+
+class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+    """View to retrieve, update, or delete a book."""
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]  # Only authenticated users can modify
