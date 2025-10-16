e8632a955e04c6c1ebfe558da448fb3011f55e89
moustapha7200@gmail.com <moustapha7200@gmail.com>
2025-10-16 15:13:34 +0200

    new codes added

---
+++ social_media_api/posts/posts/views.py
@@ -0,0 +1,25 @@
+from rest_framework import viewsets, permissions
+from django_filters.rest_framework import DjangoFilterBackend # type: ignore
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['title', 'content']
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    def get_queryset(self):
+        return self.queryset.filter(post=self.kwargs['post_pk'])
