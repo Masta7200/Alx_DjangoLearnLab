01dd115b2b52862900ae7722f12c6155b11c5892
moustapha7200@gmail.com <moustapha7200@gmail.com>
2025-10-16 14:43:46 +0200

    new commit added

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',  # For token authentication
+    'accounts',
 ]
 
 MIDDLEWARE = [
