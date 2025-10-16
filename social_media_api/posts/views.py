90f7688e1a61d777b4bd4f9731ea00e3175b3372
moustapha7200@gmail.com <moustapha7200@gmail.com>
2025-10-16 15:27:54 +0200

    send codes

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,5 +1,5 @@
-from typing import Generic
-from rest_framework import viewsets, permissions
+from typing import List
+from rest_framework import generics, permissions, viewsets
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 
@@ -21,9 +21,11 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         return self.queryset.filter(post=self.kwargs['post_pk'])
-class FeedView(Generic.ListAPIView):
+
+class FeedView(generics.ListAPIView):  # Correcting the inheritance here
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticated]
 
-    def get_queryset(self):
+    def get_queryset(self) -> List[Post]:
+        # Fetch posts from users that the current user is following
         return Post.objects.filter(author__in=self.request.user.following.all()).order_by('-created_at')
